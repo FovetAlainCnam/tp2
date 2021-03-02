ed0082bedc3cd0780d3e072f6fae4fa1d140ab7c
Alain <alain@kinomap.net>
2021-03-02 18:25:25 +0100

    Correction

--- src/question1/PileAdaptor.java
+++ src/question1/PileAdaptor.java
@@ -1,5 +1,23 @@
 package question1;
 
-public class PileAdaptor {
+public class PileAdaptor implements PileI {
+    public StackImpl stack;
 
+    PileAdaptor(StackImpl stack) {
+        this.stack = stack;
+    }
+    @Override
+    public void empiler(Object o) {
+        this.stack.push(o);
+    }
+
+    @Override
+    public Object depiler() {
+        return this.stack.pop();
+    }
+
+    @Override
+    public boolean estVide() {
+        return this.stack.isEmpty();
+    }
 }
